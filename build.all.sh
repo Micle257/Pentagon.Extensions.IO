3af71bc7f2c0361c4d0f7bec7b7c137bfd038296
Michal Pokorný <michal.pokorny0@gmail.com>
2019-04-19 18:33:04 +0200

    redesign + build scripts

---
+++ build.all.sh
@@ -0,0 +1,4 @@
+sh /c/Users/Michal/Source/GitHub/build.sh Pentagon.Extensions.IO Pentagon.Extensions.IO
+sh /c/Users/Michal/Source/GitHub/build.sh Pentagon.Extensions.IO Pentagon.Extensions.IO.Excel
+sh /c/Users/Michal/Source/GitHub/build.sh Pentagon.Extensions.IO Pentagon.Extensions.IO.MusicXml
+sh /c/Users/Michal/Source/GitHub/build.sh Pentagon.Extensions.IO Pentagon.Extensions.IO.Wav
