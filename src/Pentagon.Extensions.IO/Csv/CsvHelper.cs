2990af8bfc87b6aba5cd34c20906f9ff014f192c
Michal Pokorny <michal.pokorny0@gmail.com>
2020-01-08 21:10:27 +0100

    add CsvHelper

---
+++ src/Pentagon.Extensions.IO/Csv/CsvHelper.cs
@@ -0,0 +1,82 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="JsonHelpers.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Extensions.IO.Csv
+{
+    using System.Collections.Generic;
+    using System.Text;
+    using JetBrains.Annotations;
+
+    /// <summary> Provides helper methods for CSV data manipulation. </summary>
+    public static class CsvHelper
+    {
+        [NotNull]
+        public static string FormatCsv([NotNull] params string[] values)
+        {
+            var sb = new StringBuilder();
+
+            var first = false;
+
+            foreach (var value in values)
+            {
+                if (!first)
+                {
+                    sb.Append("\"" + value.Replace("\"", "\"\"") + "\"");
+                    first = true;
+                    continue;
+                }
+
+                sb.Append(",\"" + value.Replace("\"", "\"\"") + "\"");
+            }
+
+            return sb.ToString();
+        }
+
+        [NotNull]
+        [ItemNotNull]
+        public static IEnumerable<string> ReadCsvLine([NotNull] string line)
+        {
+            var inValue = false;
+
+            var currentValue = new StringBuilder();
+
+            for (var index = 0; index < line.Length; index++)
+            {
+                var c = line[index];
+
+                if (c == ',')
+                {
+                    if (inValue)
+                    {
+                        currentValue.Append(c);
+                    }
+                    else
+                    {
+                        yield return currentValue.ToString();
+
+                        currentValue.Clear();
+                    }
+                }
+                else if (inValue && c == '"' && index < line.Length - 1 && line[index + 1] == '"')
+                {
+                    currentValue.Append(c);
+
+                    index++;
+                }
+                else if (c == '"')
+                {
+                    inValue = !inValue;
+                }
+                else
+                {
+                    currentValue.Append(c);
+                }
+            }
+
+            yield return currentValue.ToString();
+        }
+    }
+}
---
+++ test/Pentagon.Extensions.IO.Tests/CsvHelperTests.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="CsvHelperTests.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Extensions.IO.Tests
+{
+    using System.Linq;
+    using Csv;
+    using Xunit;
+
+    public class CsvHelperTests
+    {
+        [Fact]
+        public void ReadCsvLine_LineWithAllDifficulties_Converts()
+        {
+            // ARRANGE
+            var line = @","""",""5,3"",345,""te""""st,"","""",,";
+
+            // ACT
+            var result = CsvHelper.ReadCsvLine(line).ToList();
+
+            // ASSERT
+            Assert.Equal(8, result.Count);
+            Assert.Equal(new[] {"", "", "5,3", "345", "te\"st,", "", "", ""}, result);
+        }
+
+        [Fact]
+        public void FormatCsv_WithAllDifficulties_Converts()
+        {
+            // ARRANGE
+            var line = new[] {"", "", "5,3", "345", "te\"st,", "", "", ""};
+
+            // ACT
+            var result = CsvHelper.FormatCsv(line).ToArray();
+
+            // ASSERT
+            Assert.Equal(@""""","""",""5,3"",""345"",""te""""st,"","""","""",""""", result);
+        }
+    }
+}
